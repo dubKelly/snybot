a97adbc3652410628f92b5a0594c0b76911e0b50
Jordan Neeb <js.neeb1780@gmail.com>
2017-12-21 09:16:51 -0500

    grabbing articles

--- buy.js
+++ buy.js
@@ -1,11 +1,5 @@
 var casper = require('casper').create();
 
-// page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
-
-// page.onConsoleMessage = function(msg) {
-// 	console.log(msg);
-// };
-
 var date = new Date();
 var hr = date.getHours();
 var min = date.getMinutes();
@@ -14,18 +8,27 @@ var ms = date.getMilliseconds();
 
 var drop = (3600000 * (10 - hr)) + (60000 * (60 - min)) + (1000 * (60 - sec)) + 1000 - ms;
 
-var hostName = "www.supremenewyork.com";
+var hostName = "https://www.supremenewyork.com";
 var pathName = "/shop/all";
 var href = hostName + pathName;
 
-// grab old products
+//TODO: set timeout to drop
+setTimeout(function() {
+	casper.start();
+	casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36');
 
-casper.start(href, function() {
-	this.echo("access granted");
-});
+	casper.thenOpen(href, function() {
+		this.evaluate(function() {
+			var articles = document.getElementsByTagName('article');
+			for (var i = 0; i < articles.length; i++) {
+				this.echo(this.getElementAttributes(articles[i]));
+			}
+		});
+	});	
 
-casper.then(function() {
-	casper.exit();
-})
+	casper.then(function() {
+		casper.exit();
+	});
 
-casper.run();
+	casper.run();
+}, 1000);
